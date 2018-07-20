c19af5370031bffaf4ea234239c38365a82404be
matthew rich <matt@matthews-MacBook-Pro.local>
2018-07-20 17:01:01 -0500

    complelely done besides few style changes

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ const {getMovies} = require('./api.js');
 
 $(".container").show();
 $("#form").hide();
+$(".heading").hide()
+
 // $("#editInput").hide();
 // $("#editRating").hide();
 
 function buildHtml(arrOfObj) {
     let html = "<div class='table-responsive-sm'><table class='table table-striped table-dark table-hover'>";
-    html += `<h1 class="text-light text-center">MOVIES.ORG</h1>`;
     html += "<tr>";
     html += "<th>Movie Name</th>";
     html += "<th>Movie Rating</th>";
@@ -30,14 +31,13 @@ function buildHtml(arrOfObj) {
         html += "<td>" + movie.id + "</td>";
         html += "<td class='editCol'><button class='editBtns'>Edit</button><a href='#'><i class='fa fa-trash-o deleteBtn' style='font-size:24px'></i></a></td>";
         html += "</tr>";
-        html += "<tr class='editRow'>";
+        html += "<tr class='editRow displayNone'>";
         html += `<td><input data-id=${movie.id} value=${movie.title}></td>`;
         html += `<td><input value=${movie.rating}></td>`;
         html += "<td></td>";
         html += "<td><i class='saveBtns fa fa-save' style='font-size:24px'></i></td>";
         html += "</tr>";
     });
-    html += "</table></table>";
     return html;
 }
 
@@ -45,7 +45,7 @@ function buildHtml(arrOfObj) {
 getMovies().then((data) => $(".JsonTable").html(buildHtml(data)))
     .then(() => $(".loading-container").hide())
     .then(() => $("#form").show())
-    // .then(() => $(".editRow").hide())
+    .then(() => $(".heading").show())
     .catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
         console.log(error);
@@ -56,6 +56,7 @@ let addMovie = () => {
         $(".loading-container").show();
         $("#form").hide();
         $(".JsonTable").hide();
+        $(".heading").hide();
         let movieTitleVal = $('#movieTitle').val();
         let movieRatingVal = $('#movieRating').val();
 
@@ -73,6 +74,7 @@ let addMovie = () => {
                 $(".JsonTable").show();
                 $("#form").show();
                 $(".loading-container").hide();
+                $(".heading").show()
             })
             })
             .catch(() => console.log("error!"));
@@ -82,6 +84,7 @@ let addMovie = () => {
 let editMovie = () => {
     $(document).on('click', '.editBtns', (e) => {
         $(e.currentTarget).closest('tr').next().toggleClass('visible');
+        $(e.currentTarget).parent().parent().next().toggle("displayNone")
     });
 };
 
@@ -91,6 +94,7 @@ let updateMoveAfterEdit = () => {
         $(".loading-container").show();
         $("#form").hide();
         $(".JsonTable").hide();
+        $(".heading").hide()
         let movieId = parseInt($(e.currentTarget).parent().prev().prev().prev().children().data("id"));
         console.log(movieId);
         let editTitle = $(e.currentTarget).parent().prev().prev().prev().children().val();
@@ -110,7 +114,8 @@ let updateMoveAfterEdit = () => {
                 getMovies().then((data) => $(".JsonTable").html(buildHtml(data))).then(() => {
                     $(".JsonTable").show();
                     $(".loading-container").hide();
-                    $("#form").show()
+                    $("#form").show();
+                    $(".heading").show();
                 });
 
             })
@@ -123,6 +128,7 @@ let deleteMovie = () => {
         $(".loading-container").show();
         $("#form").hide();
         $(".JsonTable").hide();
+        $(".heading").hide();
         let movieId = $(e.currentTarget).parent().parent().prev().html();
         console.log(movieId);
         let editTitle = $(e.currentTarget).parent().prev().prev().prev().children().val();
@@ -142,6 +148,7 @@ let deleteMovie = () => {
                     $(".JsonTable").show();
                     $(".loading-container").hide();
                     $("#form").show();
+                    $(".heading").show()
                 })
             })
             .catch(() => console.log("error!"));
